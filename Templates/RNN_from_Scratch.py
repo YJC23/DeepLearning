d6612a93e1ed516fe78808fc6512b8c1a1c95c2f
YJC23 <66464055+YJC23@users.noreply.github.com>
2022-08-17 17:23:49 +0900

    Create RNN_from_Scratch.py

---
+++ Templates/RNN_from_Scratch.py
@@ -0,0 +1,18 @@
+# Create Recurrent Neural Network 
+class RNN(nn.Module):
+    # implement RNN from scratch rather than using nn.RNN
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.input2hidden = nn.Linear(input_size + hidden_size, hidden_size)
+        self.input2output = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+        
+    def forward(self, input_tensor, hidden_tensor):
+        combined = torch.cat((input_tensor, hidden_tensor), 1)
+        
+        hidden = self.input2hidden(combined)
+        output = self.input2output(combined)
+        output = self.softmax(output)
+        return output, hidden
